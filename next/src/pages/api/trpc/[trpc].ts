a95ebefec126d928a157a9222a3a09adf5d62dd9
SreeCharan Desu <o210008@rguktong.ac.in>
2026-04-02 05:57:01 +0530

    docs: add TSDoc for SECRET

--- next/src/pages/api/trpc/[trpc].ts
+++ next/src/pages/api/trpc/[trpc].ts
@@ -1,6 +1,9 @@
 import { createNextApiHandler } from '@trpc/server/adapters/next';
 import { appRouter } from '../../../server';
 import jwt from "jsonwebtoken";
+/**
+ * SECRET utility.
+ */
 export const SECRET = 'SECr3t';
 import { User , Todo} from "../../../server/db";
 
